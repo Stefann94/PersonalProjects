28ec0b82b0d9b57db971209bd4d7ee83332f3f82
Stefan <stefancozma94@gmail.com>
2026-07-18 12:15:38 +0300

    primul commit cu toate proiectele personale

---
+++ PortofoliuSite/script.js
@@ -0,0 +1,13 @@
+const handleMouseMove = (e) => {
+    const { currentTarget: target } = e;
+    const rect = target.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    target.style.setProperty('--mouse-x', `${x}px`);
+    target.style.setProperty('--mouse-y', `${y}px`);
+};
+
+for (const card of document.querySelectorAll('.project-card')) {
+    card.addEventListener('mousemove', handleMouseMove);
+}
